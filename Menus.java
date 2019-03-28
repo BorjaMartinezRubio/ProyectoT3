1d62540bd338066ff5fd78f17605d0675eb769c3
BorjaMartinezRubio <47325350+BorjaMartinezRubio@users.noreply.github.com>
2019-03-28 16:23:56 +0100

    Creacion Menu Principal
    Se ha implementado el menu que aparecera al principio de la aplicacion

--- Menus.java
+++ Menus.java
@@ -1,4 +1,13 @@
 
 public class Menus {
+	
+	protected void menuInicio (){
+		
+		System.out.println("|-----------|");
+		System.out.println("|1.Empleado |");
+		System.out.println("|2.Cliente  |");
+		System.out.println("|3.Salir    |");
+		System.out.println("|-----------|");
+	}
 
 }
